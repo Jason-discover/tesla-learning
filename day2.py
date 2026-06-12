aca1c4e5d09700f6d1744fce48e28424ae8bbf02
Jason-discover <2897192673@qq.com>
2026-06-12 21:22:00 +0800

    update day2.py

--- day2.py
+++ day2.py
@@ -2,12 +2,20 @@
 ##print("Hello GitHub")
 ##print("Jason-discover")
 # print("My name is Jason")
-name = "Jason"
-# print(name)
-age = 21
-# print(age)
-major = "software Engineering"
-# print(major)
+# name = "Jason"
+# # print(name)
+# age = 21
+# # print(age)
+# major = "software Engineering"
+# # print(major)
+# print(f"My name is {name}")
+# print(f"I am {age} year old")
+# print(f"My major is {major}.")
+name = "CHENJINAN"
+school = "Yuzhang normal school"
+Dream = "Moneny"
+goal = "Having a factory like Tasla"
 print(f"My name is {name}")
-print(f"I am {age} year old")
-print(f"My major is {major}.")
+print(f"My school is {school}")
+print(f"My Dream is {Dream}")
+print(f"My goal is {goal}")
