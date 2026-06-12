28a9a624c048b121a976e2d9d8abb405e05a9eec
Jason-discover <2897192673@qq.com>
2026-06-12 21:02:07 +0800

    update day2.py

--- day2.py
+++ day2.py
@@ -1,3 +1,13 @@
-print("Hello Tesla")
-print("Hello GitHub")
-print("Jason-discover")
+##print("Hello Tesla")
+##print("Hello GitHub")
+##print("Jason-discover")
+# print("My name is Jason")
+name = "Jason"
+# print(name)
+age = 21
+# print(age)
+major = "software Engineering"
+# print(major)
+print(f"My name is {name}")
+print(f"I am {age} year old")
+print(f"My major is {major}.")
