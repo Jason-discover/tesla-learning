c9446c30b44cf18fab958973e87c89bba18ab79d
Jason-discover <2897192673@qq.com>
2026-06-13 11:08:50 +0800

    DeReorganize project structure

---
+++ python/day6.py
@@ -0,0 +1,21 @@
+# age = int(input("How old are you?"))
+
+# if age >=18:
+#     print("You are an adult.")
+# else:
+#     print("Child")
+# age = int(input("please enter your age: "))
+# if age >=18:
+#     print("You can drive")
+# else:
+#     print("you cannot drive")
+# number = int(input("please enter number: "))
+# if number> 0:
+#     print("psoitive")
+# else:
+#     print("not positive")
+score = int(input("please enter score: "))
+if score >=60:
+    print("pass")
+else:
+    print("Fail")
