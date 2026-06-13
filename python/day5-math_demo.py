f88c22cba3c378f810c88b31823ad8137a46f1c1
Jason-discover <2897192673@qq.com>
2026-06-13 10:48:26 +0800

    DeReorganize project structure

---
+++ python/day5-math_demo.py
@@ -0,0 +1,11 @@
+# a = 10
+# b = 20
+
+# print(a+b)
+# print(a*b)
+# print(b/a)
+a = int(input("First number: "))
+b = int(input("Second number: "))
+print(f"int sum = {a+b}")
+print(f"int Difference = {a-b}")
+print(f"int product = {a*b}")
---
+++ python/day5.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+major = input("What is your major?")
+Dream  = input("what is your Dream?")
+
+print(f"my name is {name}")
+print(f"my major is {major}")
+print(f"my dream is {Dream}")
