6ad76bce4bf8c50ec472eef399c88130a6ea3fde
Jason-discover <2897192673@qq.com>
2026-06-13 09:37:32 +0800

    Reorganize project structure

---
+++ python/day4.py
@@ -0,0 +1,9 @@
+name = "Your Name"
+age = 22
+major = "English"
+dream_company = "Tesla"
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"My major is {major}.")
+print(f"My dream company is {dream_company}.")
+print("I will keep learning every day.")
