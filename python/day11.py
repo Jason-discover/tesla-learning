b16dff6ddb826e32f45a63e47009d8d674cba3e8
Jason-discover <2897192673@qq.com>
2026-06-14 17:09:55 +0800

    Reorganize project structure

--- python/day11.py
+++ python/day11.py
@@ -10,3 +10,4 @@ for i in range(1,10):
     for j in range(1,i+1):
         print(f"{j}*{i}={i*j}",end="\t")
     print()
+    """九九乘法表"""
---
+++ python/day12.py
@@ -0,0 +1,13 @@
+# for i in range(1,6):
+#     print("*"*i)
+# for j in range(4,0,-1):
+#     print("*"*j)
+"""for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()"""
+for i in range(0,5):
+    for j in range(5,i,-1):
+        print(j,end="")
+    print()
+"""打印倒直角三角形"""
