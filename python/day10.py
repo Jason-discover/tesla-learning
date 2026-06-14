191b68d6f462310c05d9553466b37b9777166200
Jason-discover <2897192673@qq.com>
2026-06-14 16:21:24 +0800

    Reorganize project structure

---
+++ python/day10.py
@@ -0,0 +1,35 @@
+# for i in range(5):
+#     print(i)
+# for i in range(1,6):
+#     print(i)
+""""range 包含开始不包含结束"""
+# for i in range(2,11,2):
+#     print(i)
+""""指定步长 最后的2 是步长（间隔）"""
+# for i in range(5,0,-1):
+#     print(i)
+
+# for i in range(1,6):
+#     print(i)
+# for i in range(2,12,2):
+#     print(i)
+# for i in range(10,0,-1):
+#     print(i)
+"""print("You have 5 seconds")
+for i in range(5,0,-1):
+    print(i)
+print("Boom!")"""
+
+
+# for i in range(1,6,1):
+#     print(i*"*")
+
+# for i in range(5,0,-1):
+#     print(i*"*")
+
+"""for i in range(1,6,1):
+    print(i*"#")"""
+
+for i in range(1,6,1):
+    print(str(i)*i)
+"""字符串也可以乘法"""
---
+++ python/day11.py
@@ -0,0 +1,12 @@
+# for i in range(5):
+#     for j in range(5):
+#         print("*",end="")
+#     print()
+"""表示不换行 第一波的时候用end=让*号连在一起形成*****
+第二波再用print本身默认不换行 打印5次字符串"""      
+
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(f"{j}*{i}={i*j}",end="\t")
+    print()
