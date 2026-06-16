a53ee871423942fa0bd5f126e3c61a779293bcd3
Jason-discover <2897192673@qq.com>
2026-06-16 20:25:51 +0800

    Reorganize project structure

---
+++ Python2 _AI/day19.py
@@ -0,0 +1,38 @@
+# names = ["Tom","Jack","Rose"]
+# for name in names:
+#     print("Hello",name)
+# nums = [2,4,6]
+# for num in nums:
+#     print(num)
+
+"""photos = ["001.jpg","002.jpg","003.jpg"]
+for photo in photos:
+    print("开始分析...")
+    print("AI 正在分析",photo)
+    print("分析完成\n")"""
+
+"""resumes = ["Tom.pdf","Jack.pdf","Rose.pdf"]
+for resume in resumes:
+    print("Reading",resume)
+    print("AI is scoring...")
+    print("Scoring finished\n")"""
+
+"""nums = [1,2,3]
+for num in nums:
+     print(num)
+print(num)"""
+"""运行结束后 num不消失 它还保留最后一次循环值"""
+
+# names = ["Tom","Jack","Rose"]
+# for name in names:
+#     print(name)
+# print(names)
+# print(name)
+
+nums = [1,2,3]
+total = 0
+for num in nums:
+    total = total + num
+print(total)
+print(num)
+print(nums)
